537f62e235e2d8f04149ca83d97e2282b11f1554
sushini101 <68931594+sushini101@users.noreply.github.com>
2020-10-14 20:50:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var ball;
+var database, pos;
+
+function setup(){
+    createCanvas(500,500);
+    
+    database=firebase.database();
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition=database.ref('ball/position');
+    ballPosition.on("value",readPosition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   database.ref('ball/position').set({
+       'x': pos.x+x,
+       'y': pos.y+y
+   })
+    
+}
+
+function readPosition(data){
+    pos=data.val();
+    ball.x=pos.x;
+    ball.y=pos.y;
+
+}
